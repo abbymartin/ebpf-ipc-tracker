334292fb998ed8d73f9163df84b9a81dc25498b1
Abby Martin <abbyvmartin51@gmail.com>
2025-09-07 18:27:39 +0000

    detect unix sockets

--- pipe_tracker.c
+++ pipe_tracker.c
@@ -5,6 +5,8 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
+#define AF_UNIX 1
+
 struct event {
     u32 pid;
     //int fd;
@@ -168,7 +170,7 @@ SEC("kprobe/sys_enter_socket")
 int BPF_KPROBE(kprobe_socket, struct pt_regs *regs) {
     u32 pid = bpf_get_current_pid_tgid();
 
-    bpf_printk("socket");
+    //bpf_printk("socket");
 
     return 0;
 }
@@ -178,7 +180,27 @@ SEC("kprobe/sys_enter_connect")
 int BPF_KPROBE(kprobe_connect, struct pt_regs *regs) {
     u32 pid = bpf_get_current_pid_tgid();
 
-    bpf_printk("connect (client)");
+    //from connect() manpage
+
+    // int connect(int sockfd, const struct sockaddr *addr,
+    //                socklen_t addrlen);
+
+    // struct sockaddr {
+    //        sa_family_t     sa_family;      /* Address family */
+    //        char            sa_data[];      /* Socket address */
+    //    };
+
+    int sockfd = PT_REGS_PARM1_CORE(regs);
+    struct sockaddr *addr = (struct sockaddr *)PT_REGS_PARM2_CORE(regs);
+    //struct sockaddr *sa = (struct sockaddr *)sockaddr;
+    //sa_family_t family = sock_addr->sa_family;
+
+    u16 sa_family = 0;
+    bpf_probe_read(&sa_family, sizeof(sa_family), &addr->sa_family);
+
+    if (sa_family == AF_UNIX) {
+        bpf_printk("unix, pid = %d", pid);
+    }
 
     return 0;
 }
